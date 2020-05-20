7107fcb47c35f47591dc89741fe0d55d62c44e45
Keyeric <keyeric.bristol@gmail.com>
2020-05-20 16:34:39 -0700

    IDEK

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,15 +5,96 @@ module.exports = {
   get,
 };
 
+/*
+const item = {
+  name: "Iron Sword",
+  durability: 47,
+  enhancement: 3,
+};
+*/
+
 function succeed(item) {
-  return { ...item };
+  /*
+  When enhancement succeeds
+The item's enhancement increases by 1.
+If the item enhancement level is 20, the enhancement level is not changed.
+The durability of the item is not changed.
+*/
+  const newItem = {
+    ...item,
+    enhancement: item.enhancement + 1,
+  };
+
+  if (item.enhancement >= 0 && item.enhancement < 20) {
+    // console.log("Enhancement increased");
+    return newItem;
+  } else if (item.enhancement == 20) {
+    // console.log("Enhancement not changed");
+    return item;
+  } else if (item.enhancement < 0 || item.enhancement > 20) {
+    // console.log("Not Valid");
+    return null;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  /*
+  When enhancement fails
+If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17). 
+*/
+  if (item.enhancement <= 14) {
+    console.log("< 14", item.enhancement);
+    if (item.durability <= 100 && item.durability >= 5) {
+      console.log(item.durability);
+      return {
+        ...item,
+        durability: item.durability - 5,
+      };
+    } else if (item.durability <= 100 && item.durability < 5) {
+      return {
+        ...item,
+        durability: 0,
+      };
+    }
+  } else if (item.enhancement >= 15) {
+    console.log(">= 15", item.enhancement);
+    if (item.durability <= 100 && item.durability >= 10) {
+      return {
+        ...item,
+        durability: item.durability - 10,
+      };
+    } else if (item.durability <= 100 && item.durability < 10) {
+      return {
+        ...item,
+        durability: 0,
+      };
+    }
+  } else if (item.enhancement == 17) {
+    console.log(">16", item.enhancement);
+    if (item.durability <= 100 && item.durability >= 10) {
+      return {
+        ...item,
+        enhancement: item.enhancement - 1, //Error here, pls fix later
+        durability: item.durability - 10,
+      };
+    } else if (item.durability <= 100 && item.durability < 10) {
+      return {
+        ...item,
+        enhancement: item.enhancement - 1, //error here, pls fix later
+        durability: 0,
+      };
+    }
+  } else if (item.durability < 0 || item.durability > 100) {
+    return null;
+  }
 }
 
 function repair(item) {
+  /*
+   Return durability to 100
+  */
   return { ...item };
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,89 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require("./enhancer");
+
 // test away!
+
+const item = {
+  name: "Iron Sword",
+  durability: 50,
+  enhancement: 14,
+};
+
+describe("Enhancer", () => {
+  describe("Succeed()", () => {
+    it("should return a new item object modified according to the rules defined by the client for enhancement success", () => {
+      const yeet = succeed(item);
+
+      expect(yeet.enhancement).toBe(item.enhancement + 1);
+      expect(yeet.enhancement).toBeTruthy();
+    });
+
+    it("should not return wrong values", () => {
+      const yeet = succeed(item);
+
+      expect(yeet.enhancement).not.toBe(item.enhancement++);
+      expect(yeet.enhancement).not.toBe(item.enhancement - 2);
+      expect(yeet.enhancement).not.toBeNull();
+      expect(yeet.enhancement).not.toBeFalsy();
+    });
+  });
+
+  describe("Fail()", () => {
+    const dang = fail(item);
+    it("should pass when it is lower than 15", () => {
+      if (item.enhancement <= 14) {
+        if (item.durability >= 5) {
+          console.log("item, dang", item.durability, dang.durability);
+          expect(dang.durability).toBe(item.durability);
+        } else if (item.durability < 5) {
+          expect(dang.durability).toBe(0);
+        }
+      }
+    });
+
+    it("should pass when it is 15 or higher", () => {
+      if (item.enhancement > 14) {
+        if (item.durability > 9) {
+          expect(dang.durability).toBe(item.durability);
+        } else if (item.durability < 10) {
+          expect(dang.durability).toBe(0);
+        }
+      }
+    });
+
+    it("should pass when it is higher than 16", () => {
+      if (item.enhancement > 17) {
+        //Enhancement range from 0-20
+        expect(dang.enhancement).toBe(item.enhancement); // FIX LATER, NEEDS TO SUBTRACT ONE
+        if (item.durability > 10) {
+          //durability range from 0-100
+          //   expect(dang.enhancement).toBe(item.enhancement - 1); // FIX LATER, NEEDS TO SUBTRACT ONE
+
+          //   console.log("10 or higher enh", item.enhancement);
+          //   console.log("10 or higher dang enh", dang.enhancement);
+          //   console.log("10 or higher dur", item.durability);
+          //   console.log("10 or higher dang dur", dang.durability);
+
+          expect(dang.durability).toBe(item.durability - 10);
+        } else if (item.durability <= 10) {
+          //   expect(dang.enhancement).toBe(item.enhancement - 1); // FIX LATER, NEEDS TO SUBTRACT ONE
+
+          //   console.log("lower than 10 enh", item.enhancement);
+          //   console.log("lower than 10 dang enh", dang.enhancement);
+          //   console.log("lower than 10 dur", item.durability);
+          //   console.log("lower than 10 dang dur", dang.durability);
+
+          expect(dang.durability).toBe(0);
+        }
+        console.log("higher than 16", dang);
+      }
+    });
+  });
+
+  describe("Repair()", () => {
+    it("returns a new item with the durability restored to 100.", () => {});
+  });
+
+  describe("Get()", () => {
+    it("returns a new item with the name property modified according to the following rules: if the enhancement level is 0, the the name is not modified; if the enhancement level is greater than 0, change the name to include the enhancement level, preceded by a plus sign ( + ), between square brackets before the item's name.", () => {});
+  });
+});
